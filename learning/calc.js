f6ee2a518d684b46e82bd4817463aea50407ae30
adamochi <101391749+adamochi@users.noreply.github.com>
2022-04-30 18:49:33 +0900

    2.10 recap

--- learning/calc.js
+++ learning/calc.js
@@ -20,3 +20,17 @@ amIFat = false;
 
 
 // we are not creating the variable, we are updating the varieable
+
+
+//  TRUE & FALSE are booleans
+//  null is an asigned value. when we want to literally say here there is nothing, Nothing IS something.
+// undefined is something that has no value (like an empty container), different to null. because null is something.
+// To create an array, you just need [] square  brackets
+const me = "sexy";
+const days = [1, 2, false, true, null, undefined, "text", me];
+const toPurchase = ["banana", "tomatoes"]
+toPurchase.push("pizza");
+console.log(toPurchase[2]);
+console.log(toPurchase[1]);
+toPurchase[1] = "water";
+console.log(toPurchase);
