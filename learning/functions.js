ce77a53449d153ea528613fee08d6f2a18592db5
adamochi <honeyrumballs@gmail.com>
2022-05-06 15:19:14 +1000

    2.11 returns

--- learning/functions.js
+++ learning/functions.js
@@ -26,3 +26,46 @@ console.log(playa.name);
 playa.sayHelloo("chloe");
 
 // ^^ have own console.log() | array.push() ^^
+function banana() {
+    console.log("ban" + "ana");
+}
+
+// recap ii 
+// need to add () to press play on the function
+banana();
+banana();
+banana();
+banana();
+banana();
+banana();
+banana();
+plus();
+plus();
+plus();
+plus();
+plus();
+plus();
+
+
+// we leart how to send data to functions before we learned how to receive data from functions
+
+// need to be able to send data to functions
+plus(4, 5);
+plus(987.99, 203);
+
+const calculator = {
+    add: function (a, b) {
+    console.log(a + b);
+},  minus: function (a, b) {
+    console.log(a - b);
+},  divide: function (a, b) {
+    console.log(a / b);
+},  power: function (a, b) {
+    console.log(a**b);
+}
+};
+
+calculator.add(5, 100);
+calculator.minus(100, 70);
+calculator.divide(5, 10);
+calculator.power(645, 3);
