44517a1aca2aab07c70d132d10e062322ffd1819
adamochi <honeyrumballs@gmail.com>
2022-05-16 22:00:01 +1000

    random game

--- app.js
+++ app.js
@@ -3,22 +3,47 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 // can search inside of element or just do like this^^
 //to trigger the validation of an input on html specifically, it must be inside of a form!!!!!!!!!!!!!!!!!!!!!!!
 const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   console.log(username);
+  paintGreetings(username);
 }
+
+function paintGreetings(username) {
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greeting
+  paintGreetings(savedUsername);
+}
+
+/*
 function handleLinkClick(event) {
   event.preventDefault();
   console.log(link.href);
+  console.log(event);
 }
-
-loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLinkClick);
+*/
 
 //   console.log(loginInput.value);
 //   console.log("please write your name");
--- randomnumber/src/app.js
+++ randomnumber/src/app.js
@@ -1,14 +1,114 @@
 const numberForm = document.querySelector("#numberGame");
-const numberRange = document.querySelector("#numberGame:first-child input");
-const userGuess = document.querySelector("#numberGame:last-child input");
+const numberRange = document.querySelector("#numberGame .range");
+const userGuess = document.querySelector("#numberGame .guess");
+const youChose = document.querySelector(".you-chose");
+const machineChose = document.querySelector(".machine-chose");
+const randomDisplay = document.querySelector(".random-number");
+const winDisplay = document.querySelector(".hidden");
+const loseDisplay = document.querySelector(".hidden-lose");
 
-const randomNumber = numberRange[Math.floor(Math.random() * numberRange)];
+if (localStorage !== null) {
+  localStorage.clear();
+}
 
-function rangeSubmitted(tomato) {
-  tomato.preventDefault();
-  const maxRange = numberRange.value;
+const myMegaSexyRandomHandler = {
+  rangeSubmitted: function (event) {
+    event.preventDefault();
+    const maxRange = numberRange.value;
+    localStorage.setItem("maxRange", maxRange);
+    return maxRange;
+  },
+  guessSubmitted: function (event) {
+    const guessedNumber = userGuess.value;
+    localStorage.setItem("guessedNumber", guessedNumber);
+    youChose.innerText = "You Chose: " + guessedNumber;
+    return guessedNumber;
+  },
+  randomNumberfun: function (event) {
+    const randomNumber = Math.floor(Math.random() * numberRange.value);
+    localStorage.setItem("randomNumber", randomNumber);
+    randomDisplay.innerText = "The Number is: " + randomNumber;
+    return randomNumber;
+  },
+  machineChoice: function (event) {
+    const aiChoice = Math.floor(Math.random() * numberRange.value);
+    localStorage.setItem("aiChoice", aiChoice);
+    machineChose.innerText = "The Machine Chose: " + aiChoice;
+    return aiChoice;
+  },
+};
+
+numberForm.addEventListener("submit", myMegaSexyRandomHandler.rangeSubmitted);
+numberForm.addEventListener("submit", myMegaSexyRandomHandler.randomNumberfun);
+numberForm.addEventListener("submit", myMegaSexyRandomHandler.guessSubmitted);
+numberForm.addEventListener("submit", myMegaSexyRandomHandler.machineChoice);
+
+const maxRangeResult = localStorage.getItem("maxRange");
+
+function playIt() {
+  const maxRange = localStorage.getItem("maxRange");
+  const randomNumber = localStorage.getItem("randomNumber");
+  const guessedNumber = localStorage.getItem("guessedNumber");
+  const aiChoice = localStorage.getItem("aiChoice");
+  //   console.log(maxRange, randomNumber, guessedNumber, aiChoice);
+  if (guessedNumber === randomNumber && aiChoice === randomNumber) {
+    console.log("double winners");
+  } else if (guessedNumber === randomNumber) {
+    console.log("winna");
+    winDisplay.classList.remove("hidden");
+    loseDisplay.classList.add("hidden-lose");
+  } else if (aiChoice === randomNumber) {
+    console.log("loser");
+    winDisplay.classList.add("hidden");
+    loseDisplay.classList.remove("hidden-lose");
+  } else if (guessedNumber !== randomNumber) {
+    console.log("keep partying");
+    loseDisplay.classList.add("hidden-lose");
+    winDisplay.classList.add("hidden");
+  } else if (aiChoice !== randomNumber) {
+    loseDisplay.classList.add("hidden-lose");
+  }
+}
+
+numberForm.addEventListener("submit", playIt);
+//   if (guessedNumber === randomNumbe && guessedNumber !== randomNumber) {
+//     winDisplay.classList.remove("hidden");
+//   } else if (aiChoice === randomNumber) {
+//     loseDisplay.classList.remove("hidden-lose");
+//   } else {
+//     winDisplay.classList.add("hidden");
+//     loseDisplay.classList.add("hidden-lose");
+//   }
+// }
+
+/*
+function rangeSubmitted(event) {
+  event.preventDefault();
+  const maxRange = parseInt(numberRange.value);
   // const maxRange = parseInt(numberRange.value);
   console.log("Max range chosen " + maxRange);
 }
+function guessSubmitted(event) {
+  event.preventDefault();
 
-numberForm.addEventListener("submit", rangeSubmitted);
+  const guessedNumber = parseInt(userGuess.value);
+  console.log("You Chose: " + guessedNumber);
+}
+function randomNumberfun(event) {
+  event.preventDefault();
+  const randomNumber = Math.floor(Math.random() * numberRange.value);
+  console.log(randomNumber);
+}
+function machineChoice(event) {
+  event.preventDefault();
+  const aiChoice = Math.floor(Math.random() * numberRange.value);
+  console.log("the machine chose " + aiChoice);
+}
+function didYouWin (event) {
+    if (guessedNumber === randomNumber) {
+        console.log("you win");
+    } else (aiChoice === randomNumber) {
+        console.log("you lose")
+    }
+}
+*/
