57882dbb374ba2c238f61fd02e7980588565b70b
adamochi <honeyrumballs@gmail.com>
2022-06-20 11:20:01 +1000

    what the heck is deep equal.......

--- learning/document.js
+++ learning/document.js
@@ -5,3 +5,9 @@
 // document.title = "Hi"
 document.title = "Hello, from JS!";
 // the document is literally your website
+function mergeArrays(arr1, arr2) {
+  let arr3 = new Uint8Array(arr1.concat(arr2)).sort();
+  return arr3;
+}
+
+console.log(mergeArrays([5, 6, 7, 8], [1, 2, 3, 4]));
